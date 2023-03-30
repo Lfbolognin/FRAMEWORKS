323455cb64fe7c6f67ac0fb871a5979a71fd3621
Lfbolognin <126827723+Lfbolognin@users.noreply.github.com>
2023-03-30 18:23:09 -0400

    Add files via upload

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
+<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
@@ -20,3 +21,21 @@ import { LoginComponent } from './components/login/login.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+=======
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+>>>>>>> 3424ad5621dc37ce1e246ea4dfb6c411f2e2e24a
