67ed931e823834812a46250c44105af0f1996596
Lfbolognin <126827723+Lfbolognin@users.noreply.github.com>
2023-06-13 15:57:04 -0400

    Add files via upload

---
+++ app/components/task/task.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+
+tasks= []
+
+
+task = new FormControl('')
+descricao = new FormControl('')
+responsavel = new FormControl('')
+dt_inicio = new FormControl('')
+dt_fim = new FormControl('')
+
+addTask(){
+
+  console.log(this.task.value)
+  console.log(this.descricao.value)
+  console.log(this.responsavel.value)
+  console.log(this.dt_inicio.value)
+  console.log(this.dt_fim.value)
+}
+
+
+
+removeTask( ){}
+
+
+}
