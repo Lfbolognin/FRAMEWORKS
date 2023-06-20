061d1c5ad7974acc72d11229362f2dd9ed5d60bd
Lfbolognin <126827723+Lfbolognin@users.noreply.github.com>
2023-06-20 16:06:53 -0400

    Add files via upload

--- my-app/src/app/components/task/task.component.ts
+++ my-app/src/app/components/task/task.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Task } from 'src/app/model/task';
 
 @Component({
   selector: 'app-task',
@@ -8,24 +9,41 @@ import { FormControl } from '@angular/forms';
 })
 export class TaskComponent {
 
-tasks= []
+nome = new FormControl('');
+descricao = new FormControl('');
+responsavel = new FormControl('');
+dt_inicio = new FormControl('');
+dt_fim = new FormControl('');
 
-
-task = new FormControl('')
-descricao = new FormControl('')
-responsavel = new FormControl('')
-dt_inicio = new FormControl('')
-dt_fim = new FormControl('')
+tasks: Task[] = []
 
 addTask(){
-
-  console.log(this.task)
-
+let t = this.dataToObject();
+this.tasks.push(t);
+console.log(t);
+this.limparForm();
 }
 
+dataToObject(){
 
+  let task = new Task() // instaciasmos um objeto
+  task.nome = this.nome.value!;
+  task.descricao = this.descricao.value!;
+  task.responsavel  = this.responsavel.value!;
+  task.dt_inicio  = Number(this.dt_inicio.value)!;
+  task.dt_fim  = Number(this.dt_fim.value)!;
 
+  return task;
+}
 
+limparForm(){
+
+  this.nome.setValue('');
+  this.descricao.setValue('');
+  this.responsavel.setValue('');
+  this.dt_inicio.setValue('');
+  this.dt_fim.setValue('');
+};
 
 
 }
---
+++ my-app/src/app/model/task.ts
@@ -0,0 +1,29 @@
+export class Task{
+
+//atributo
+nome: string;
+descricao: string;
+responsavel: string;
+dt_inicio: number;
+dt_fim: number;
+
+
+
+
+constructor(){
+
+    this.nome ="";
+    this.descricao ="";
+    this.responsavel ="";
+    this.dt_inicio = 0;
+    this.dt_fim = 0;
+
+}
+
+
+
+
+
+
+
+}
